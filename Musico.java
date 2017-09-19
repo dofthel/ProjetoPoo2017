d2a44d885a2d685242e03c56c8e65bd1b2c35ee8
dofthel <31527800+dofthel@users.noreply.github.com>
2017-09-19 15:24:51 -0300

    Classe Musico extensão de Usuario

---
+++ Musico.java
@@ -0,0 +1,21 @@
+package solfazendomusica;
+
+public class Musico extends Usuario {
+	String instrumentoMusical;
+	
+	public Musico(String nome, String nickname, String email, String senha, String confirmarSenha){
+		//super(nome, nickname, email, senha, confirmarSenha);
+	}
+	
+	public void receber(){
+		
+	}
+	
+	public void receber(String nome, String nickname,String email) {
+		
+	}
+	
+	public void tocar() {
+		
+	}
+}
